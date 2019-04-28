cc57b016483db4d395cea35ee65e160fc6cdcda4
Tim Strasser <tim.strasser@uzh.ch>
2019-04-28 23:12:50 +0200

    Delete unnecessary methods from contract

--- src/contracts/DLottery_2.0.2.sol
+++ src/contracts/DLottery_2.0.2.sol
@@ -126,47 +126,5 @@ contract DLottery {
         block_numbers.push(block.number);
         block_difficulties.push(block.difficulty);
     }
-    
-    function mergeBytes(bytes memory param1, bytes memory param2, bytes memory param3) private pure returns (bytes memory) {
-        bytes memory merged = new bytes(param1.length + param2.length + param3.length);
-
-        uint k = 0;
-        for (uint i = 0; i < param1.length; i++) {
-            merged[k] = param1[i];
-            k++;
-        }
-    
-        for (uint i = 0; i < param2.length; i++) {
-            merged[k] = param2[i];
-            k++;
-        }
-        
-        for (uint i = 0; i < param3.length; i++) {
-            merged[k] = param3[i];
-            k++;
-        }
-        
-        return merged;
-    }
-    
-    function toBytes(uint8 x) private pure returns (bytes memory b) {
-        b = new bytes(1);
-        b[0] = byte(x);
-        return b;
-    }
-    
-    function toBytes(address x) private pure returns (bytes memory b) {
-        b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-        return b;
-    }
-    
-    function toString(address x) private pure returns (string memory) {
-        bytes memory b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-        return string(b);
-    }
 }
     
