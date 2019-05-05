17c303df38003012bad9ee373903e51c9bedf4cc
Tim Strasser <tim.strasser@uzh.ch>
2019-05-05 10:13:38 +0200

    Add test

--- test/TestDLottery.js
+++ test/TestDLottery.js
@@ -6,4 +6,11 @@ contract("DLottery", accounts => {
         let phase = await instance.current_phase.call();
         assert.equal(phase, 0);
     });
+
+    it('should be able to commit correctly', async(web3) => {
+        let instance = await DLottery.deployed();
+        let entry_fee = await instance.getFee.call();
+        let hash = web3.eth.util.soliditySha3('test');
+        await instance.commit.call("sdsd", {value: entry_fee});
+    });
 });
