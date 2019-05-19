0fdade0d9d5943434ac94300565069544956cb38
Tim Strasser <tim.strasser@uzh.ch>
2019-05-19 15:28:53 +0200

    Fix tests

--- test/TestDLottery.js
+++ test/TestDLottery.js
@@ -6,19 +6,31 @@ contract("DLottery", accounts => {
     let generator;
     let lottery;
 
-    beforeEach(async function() {
+    beforeEach(async () => {
         generator = await WinningNumbersGenerator.new();
         lottery = await DLottery.new(generator.address);
     });
 
     it("should initialize the phase correctly", async () => {
-        let phase = await lottery.getCurrentPhase.call();
+        let phase = await lottery.getCurrentPhase();
         assert.equal(phase, 0);
     });
 
     it('should be able to commit correctly', async() => {
-        let entry_fee = await lottery.getFee.call();
+        let oldBalance = await lottery.getBalance();
+        assert.equal(oldBalance, 0);
+        let entry_fee = await lottery.getFee();
         let hash = web3.utils.soliditySha3('test');
-        await lottery.commit.call(hash, {value: entry_fee});
+        await lottery.commit(hash, {value: entry_fee});
+        let newBalance = await lottery.getBalance();
+        assert.equal(web3.utils.toWei(newBalance, 'wei'), web3.utils.toWei(oldBalance + entry_fee, 'wei'));
+    });
+
+    it('should be able to load the contracts jackpot', async() => {
+        let oldBalance = await lottery.getBalance();
+        let load = web3.utils.toWei('10', 'ether');
+        await lottery.load({value: load});
+        let newBalance = await lottery.getBalance();
+        assert.equal(web3.utils.toWei(oldBalance + load, 'wei'), web3.utils.toWei(newBalance, 'wei'));    
     });
 });
